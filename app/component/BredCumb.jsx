5f5ee7ef56f172f6a4e85296c4e9996de68dfeb9
nazneen636 <nazneennahar636@gmail.com>
2024-11-08 22:17:17 +0600

    bredcumb change

--- app/component/BredCumb.jsx
+++ app/component/BredCumb.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "../layer/Container";
 import Dot from "../layer/Dot";
 import { FaCircle } from "react-icons/fa";
 
 const BredCumb = () => {
+  const [path, setPath] = useState("");
+
+  useEffect(() => {
+    // Check if window is defined and set the pathname
+    if (typeof window !== "undefined") {
+      setPath(window.location.pathname.split("/")[1]);
+    }
+  }, []);
+
   return (
     <div className="bg-bannerBg2 pt-[159px] bg-no-repeat bg-bottom bg-cover pb-[181px]">
       <Container>
         <h2 className="text-[60px] font-semibold capitalize leading-[100%] text-white font-grotesque">
-          {window.location.pathname.split("/")[1]}
+          {path}
         </h2>
         <div className="mt-[30px] flex items-center gap-3">
           <span className="capitalize font-roboto text-sm leading-[170%] text-[#B3B1B7]">
@@ -16,7 +25,7 @@ const BredCumb = () => {
           </span>
           <FaCircle className="text-[#B3B1B7] text-[4px]" />
           <span className="capitalize font-roboto text-sm leading-[170%] text-[#B3B1B7]">
-            {window.location.pathname.split("/")[1]}
+            {path}
           </span>
         </div>
       </Container>
